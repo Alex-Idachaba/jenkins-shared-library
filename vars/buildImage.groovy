d764dc8626370b3bbbb892145f5253075735c577
Ainoko Alex Idachaba <click2alex@gmail.com>
2023-11-01 14:13:49 +0100

    build image with param

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,10 +1,10 @@
 #!/usr/bin/env groovy
 
-def call() {
+def call(String imageName) {
     echo "building the docker image...."
     withCredentials([usernamePassword(credentialsId: 'dockerhub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh "docker build -t alexdevops31/jenkins-demo-app:jda-2.0 ."
+        sh "docker build -t $imageName ."
         sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh "docker push alexdevops31/jenkins-demo-app:jda-2.0"
+        sh "docker push $imageName"
     }
 }
